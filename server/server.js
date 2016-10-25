8f46714f8728f4609157e9f98ed6b84e7a4b5b29
John Contreras <contrerasnet@gmail.com>
2016-10-24 21:41:43 -0500

    add initial server api

---
+++ server/server.js
@@ -0,0 +1,25 @@
+require("babel-polyfill");
+
+const express = require('express')
+const path    = require('path')
+const app     = express()
+
+app.use('/', express.static(path.join(__dirname, '/client/build')))
+
+const routes = require('express').Router()
+
+const some = async () => {
+    console.log('nothing')
+}
+
+routes.get('/', (req, res) => {
+  res.status(200).json( {message: 'babel + cra today^^**'} )
+})
+
+app.use('/api', routes)
+
+app.set('port', (process.env.PORT || 3000))
+
+app.listen(app.get('port'), () => {
+  console.log('started on port', app.get('port'))
+})
