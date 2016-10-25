768d35a68718925ac5049e0bfd1a889b35b3c676
John Contreras <contrerasnet@gmail.com>
2016-10-24 22:38:04 -0500

    fix the path to static files

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const express = require('express')
 const path    = require('path')
 const app     = express()
 
-app.use('/', express.static(path.join(__dirname, '/client/build')))
+app.use('/', express.static(path.join(__dirname, '../client/build')));
+
 
 const routes = require('express').Router()
 
